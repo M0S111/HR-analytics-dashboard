9ded25a56f91242632d7729847dd4b859c20cbc6
M0S111 <stormbrewer@yandex.com>
2026-08-24 05:35:23 +0500

    Fix df len error

--- app.py
+++ app.py
@@ -287,10 +287,12 @@ def fetch_and_update_dashboard(n_clicks, search_term, search_country, search_cit
         if df['country'].iloc[0] != "Unknown" and len(loc_display) <= 3:
             loc_display = df['country'].iloc[0]
 
+        length = len(df)
+
         del df, data
         gc.collect()
 
-        return fig_kpi1, fig_kpi2, fig_salary, fig_titles, fig_dot, f"Data refreshed. Top {len(df)} postings in {loc_display}."
+        return fig_kpi1, fig_kpi2, fig_salary, fig_titles, fig_dot, f"Data refreshed. Top {length} postings in {loc_display}."
 
     except Exception as e:
         
